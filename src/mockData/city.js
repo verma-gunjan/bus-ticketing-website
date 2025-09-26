0bb7f2053729eaedb8a6426e173ac09ca5bcea26
Gunjan Verma <vermagunjan9927@gmail.com>
2025-09-26 14:46:08 +0530

    fix mock data

--- src/mockData/city.js
+++ src/mockData/city.js
@@ -1,73 +1,102 @@
 export const cities = [
-    {
-      "_id": "city01",
-      "name": "Delhi, Delhi",
-      "value": "Delhi",
-      "state": "Delhi",
-      "cityName": "Delhi"
-    },
-    {
-      "_id": "city02",
-      "name": "Gurgaon, Haryana",
-      "value": "Gurgaon",
-      "state": "Haryana",
-      "cityName": "Gurgaon"
-    },
-    {
-      "_id": "city03",
-      "name": "Manesar, Haryana",
-      "value": "Manesar",
-      "state": "Haryana",
-      "cityName": "Manesar"
-    },
-    {
-      "_id": "city04",
-      "name": "Dharuhera, Haryana",
-      "value": "Dharuhera",
-      "state": "Haryana",
-      "cityName": "Dharuhera"
-    },
-    {
-      "_id": "city05",
-      "name": "Neemrana, Rajasthan",
-      "value": "Neemrana",
-      "state": "Rajasthan",
-      "cityName": "Neemrana"
-    },
-    {
-      "_id": "city06",
-      "name": "Behror, Rajasthan",
-      "value": "Behror",
-      "state": "Rajasthan",
-      "cityName": "Behror"
-    },
-    {
-      "_id": "city07",
-      "name": "Kotputli, Rajasthan",
-      "value": "Kotputli",
-      "state": "Rajasthan",
-      "cityName": "Kotputli"
-    },
-    {
-      "_id": "city08",
-      "name": "Shahpura, Rajasthan",
-      "value": "Shahpura",
-      "state": "Rajasthan",
-      "cityName": "Shahpura"
-    },
-    {
-      "_id": "city09",
-      "name": "Jaipur, Rajasthan",
-      "value": "Jaipur",
-      "state": "Rajasthan",
-      "cityName": "Jaipur"
-    },
-    {
-      "_id": "city10",
-      "name": "Bilaspur Kalan, Haryana",
-      "value": "Bilaspur Kalan",
-      "state": "Haryana",
-      "cityName": "Bilaspur Kalan"
-    }
-  ]
-  
+  {
+    "_id": "city01",
+    "name": "Delhi, Delhi",
+    "value": "Delhi",
+    "state": "Delhi",
+    "cityName": "Delhi",
+    "shortName": "Delhi",
+    "searchTags": ["Dilli", "New Delhi", "NDLS"],
+    "isPopular": true
+  },
+  {
+    "_id": "city02",
+    "name": "Gurgaon, Haryana",
+    "value": "Gurgaon",
+    "state": "Haryana",
+    "cityName": "Gurgaon",
+    "shortName": "Gurgaon",
+    "searchTags": ["Gurugram", "Gurgao"],
+    "isPopular": true
+  },
+  {
+    "_id": "city03",
+    "name": "Manesar, Haryana",
+    "value": "Manesar",
+    "state": "Haryana",
+    "cityName": "Manesar",
+    "shortName": "Manesar",
+    "searchTags": ["Manesar Haryana"],
+    "isPopular": false
+  },
+  {
+    "_id": "city04",
+    "name": "Dharuhera, Haryana",
+    "value": "Dharuhera",
+    "state": "Haryana",
+    "cityName": "Dharuhera",
+    "shortName": "Dharuhera",
+    "searchTags": ["Dharuhera Haryana"],
+    "isPopular": false
+  },
+  {
+    "_id": "city05",
+    "name": "Neemrana, Rajasthan",
+    "value": "Neemrana",
+    "state": "Rajasthan",
+    "cityName": "Neemrana",
+    "shortName": "Neemrana",
+    "searchTags": ["Neemrana Rajasthan"],
+    "isPopular": false
+  },
+  {
+    "_id": "city06",
+    "name": "Behror, Rajasthan",
+    "value": "Behror",
+    "state": "Rajasthan",
+    "cityName": "Behror",
+    "shortName": "Behror",
+    "searchTags": ["Behror Rajasthan"],
+    "isPopular": false
+  },
+  {
+    "_id": "city07",
+    "name": "Kotputli, Rajasthan",
+    "value": "Kotputli",
+    "state": "Rajasthan",
+    "cityName": "Kotputli",
+    "shortName": "Kotputli",
+    "searchTags": ["Kotputali"],
+    "isPopular": false
+  },
+  {
+    "_id": "city08",
+    "name": "Shahpura, Rajasthan",
+    "value": "Shahpura",
+    "state": "Rajasthan",
+    "cityName": "Shahpura",
+    "shortName": "Shahpura",
+    "searchTags": ["Shahpura Rajasthan"],
+    "isPopular": false
+  },
+  {
+    "_id": "city09",
+    "name": "Jaipur, Rajasthan",
+    "value": "Jaipur",
+    "state": "Rajasthan",
+    "cityName": "Jaipur",
+    "shortName": "Jaipur",
+    "searchTags": ["Pink City", "Jaipor", "Jaipur City"],
+    "isPopular": true
+  },
+  {
+    "_id": "city10",
+    "name": "Bilaspur Kalan, Haryana",
+    "value": "Bilaspur Kalan",
+    "state": "Haryana",
+    "cityName": "Bilaspur Kalan",
+    "shortName": "Bilaspur Kalan",
+    "searchTags": ["Bilaspur Haryana"],
+    "isPopular": false
+  }
+]
